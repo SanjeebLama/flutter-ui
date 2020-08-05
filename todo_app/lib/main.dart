aa4e919d16bb28a1f18396dfe99957603a10696a
S J <sanjeeb.lama004@gmail.com>
2020-08-05 20:36:13 +0545

    Header Completed

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'custom_clipper.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
@@ -6,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -23,12 +24,133 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  double _imageHeight = 256.0;
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Stack(
-        children: <Widget>[Text('Initial Project')],
+        children: <Widget>[
+          _buildImage(),
+          _buildTopHeader(),
+          _buildProfileRow(),
+          _buildBottomPart(),
+        ],
       ),
     );
+  }
+
+  Widget _buildImage() {
+    return Positioned.fill(
+      bottom: null,
+      child: ClipPath(
+        clipper: new DiagonalClipper(),
+        child: new Image.asset(
+          'images/birds.jpg',
+          fit: BoxFit.cover,
+          height: _imageHeight,
+          colorBlendMode: BlendMode.srcOver,
+          color: new Color.fromARGB(120, 20, 10, 40),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTopHeader() {
+    return new Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 32.0),
+      child: new Row(
+        children: <Widget>[
+          new Icon(Icons.menu, size: 32.0, color: Colors.white),
+          new Expanded(
+            child: new Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: new Text(
+                "Timeline",
+                style: new TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300),
+              ),
+            ),
+          ),
+          new Icon(Icons.linear_scale, color: Colors.white),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileRow() {
+    return Padding(
+      padding: EdgeInsets.only(left: 16.0, top: _imageHeight / 2.5),
+      child: Row(
+        children: <Widget>[
+          CircleAvatar(
+            backgroundImage: AssetImage('images/avatar.jpg'),
+            minRadius: 28.0,
+            maxRadius: 28.0,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'John Doe',
+                  style: TextStyle(
+                      fontSize: 26.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400),
+                ),
+                Text(
+                  'Product Manager',
+                  style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBottomPart() {
+    return Padding(
+      padding: EdgeInsets.only(top: _imageHeight),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _buildTaskHeader(),
+          _buildTasksList(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTaskHeader() {
+    return Padding(
+      padding: EdgeInsets.only(left: 64.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'My Tasks',
+            style: TextStyle(fontSize: 34.0),
+          ),
+          Text(
+            'Date: Aug 8',
+            style: TextStyle(color: Colors.grey, fontSize: 12.0),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTasksList() {
+    return new Container();
   }
 }
