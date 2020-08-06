99de3ff1ff2f447284a40404c9df127d04fa8761
S J <sanjeeb.lama004@gmail.com>
2020-08-06 17:09:46 +0545

    Inital Commit

---
+++ profile_ui/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // final _imageHeight = 256.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //
+        );
+  }
+}
