3af3b807e9c1998ce35ba21e5bd0f65505e7a279
S J <sanjeeb.lama004@gmail.com>
2020-08-07 21:54:12 +0545

    Profile modified

--- profile_ui/lib/main.dart
+++ profile_ui/lib/main.dart
@@ -10,17 +10,73 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(),
+      home: Scaffold(
+        body: ProfileUI(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  // final _imageHeight = 256.0;
+class ProfileUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        //
-        );
+    return Column(
+      children: <Widget>[
+        Stack(
+          overflow: Overflow.visible,
+          alignment: Alignment.center,
+          children: <Widget>[
+            Image.asset(
+              'images/birds.jpg',
+              fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height / 3,
+            ),
+            Positioned(
+                bottom: -50.0,
+                child: CircleAvatar(
+                  radius: 80,
+                  backgroundColor: Colors.white,
+                  backgroundImage: AssetImage('images/avatar.jpg'),
+                ))
+          ],
+        ),
+        SizedBox(
+          height: 60,
+        ),
+        Center(
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Iron Man',
+                style: TextStyle(fontWeight: FontWeight.w400),
+              ),
+              Text(
+                'The one and only Tony Stark',
+                style: TextStyle(fontWeight: FontWeight.w200),
+              ),
+              Padding(
+                padding: EdgeInsets.all(24.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.contact_mail),
+                    Text('Call Me Ladies !'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Text(
+          'About me',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        Text(
+          'Hi my name is Tony Stark and I am Iron Man. What you want more? Well, I am also genius, billionare, playboy and philantropist',
+          style: TextStyle(fontStyle: FontStyle.italic, color: Colors.blueGrey),
+        ),
+      ],
+    );
   }
 }
