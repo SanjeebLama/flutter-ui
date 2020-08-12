71a5ba5ae3162c70db02881b0b463c59e4ead23c
S J <sanjeeb.lama004@gmail.com>
2020-08-12 14:46:45 +0545

    Main page updated

--- day1_ui/lib/main.dart
+++ day1_ui/lib/main.dart
@@ -1,15 +1,11 @@
-import 'package:day1_ui/home_UI/index_page.dart';
 import 'package:flutter/material.dart';
 import 'home_UI/index_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Furniture",
+    debugShowCheckedModeBanner: false,
+    title: "Blog Profile",
     home: Scaffold(
-      appBar: AppBar(
-        title: Text("Shoping Cart"),
-        backgroundColor: Colors.deepPurple,
-      ),
       body: IndexPage(),
     ),
   ));
