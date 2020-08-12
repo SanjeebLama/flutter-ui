bc29a888472fdaa0c381039a24f91e29bf536e1b
S J <sanjeeb.lama004@gmail.com>
2020-08-12 20:06:21 +0545

    Action button added

--- day1_ui/lib/main.dart
+++ day1_ui/lib/main.dart
@@ -7,6 +7,19 @@ void main() {
     title: "Blog Profile",
     home: Scaffold(
       body: IndexPage(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          null;
+           
+        },
+        child: Icon(Icons.message),
+        tooltip: "Message Box",
+      ),
     ),
   ));
+
+  void showSnackBar(BuildContext context) {
+   var snackBar = SnackBar(content: Text(" Message Button was Tapped");
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
 }
