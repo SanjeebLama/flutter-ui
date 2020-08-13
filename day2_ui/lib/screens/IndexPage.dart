ad786e8359f137f7893d4afa883a0f01b78ab66a
S J <sanjeeb.lama004@gmail.com>
2020-08-13 18:32:03 +0545

    BottomNavigationBar added

--- day2_ui/lib/screens/IndexPage.dart
+++ day2_ui/lib/screens/IndexPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './dashboard.dart';
 
 class IndexPage extends StatefulWidget {
   @override
@@ -6,8 +7,62 @@ class IndexPage extends StatefulWidget {
 }
 
 class _IndexPageState extends State<IndexPage> {
+  var selectedItem = 0;
+  List childeren = [Dashboard(), Dashboard(), Dashboard(), Dashboard()];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFF202020),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Color(0xFFD4F99),
+        iconSize: 27.0,
+        currentIndex: selectedItem,
+        unselectedLabelStyle: TextStyle(color: Color(0xFF1B1B1B)),
+        unselectedItemColor: Color(0xFF888888),
+        onTap: (currentIndex) {
+          setState(() {
+            selectedItem = currentIndex;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+              backgroundColor: Color(0xFF1B1B1B),
+              icon: Icon(Icons.home),
+              title: Container(
+                height: 5.0,
+                width: 5.0,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: Color(0xFFFD4F99)),
+              )),
+          BottomNavigationBarItem(
+              backgroundColor: Color(0xFF1B1B1B),
+              icon: Icon(Icons.search),
+              title: Container(
+                height: 5.0,
+                width: 5.0,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: Color(0xFFFD4F99)),
+              )),
+          BottomNavigationBarItem(
+              backgroundColor: Color(0xFF1B1B1B),
+              icon: Icon(Icons.camera),
+              title: Container(
+                height: 5.0,
+                width: 5.0,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: Color(0xFFFD4F99)),
+              )),
+          BottomNavigationBarItem(
+              backgroundColor: Color(0xFF1B1B1B),
+              icon: Icon(Icons.person),
+              title: Container(
+                height: 5.0,
+                width: 5.0,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, color: Color(0xFFFD4F99)),
+              )),
+        ],
+      ),
+    );
   }
 }
