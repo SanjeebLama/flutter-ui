1d62686e3c5d00b7cf8e0c30c15e8b8db9d33a69
S J <sanjeeb.lama004@gmail.com>
2020-08-13 18:59:39 +0545

    Header Completed

--- day2_ui/lib/screens/dashboard.dart
+++ day2_ui/lib/screens/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -8,6 +9,85 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 20.0,
+        ),
+        Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: 40.0,
+                width: 40.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7.0),
+                    color: Color(0xFFFD4F99)),
+                child: Center(
+                  child: Icon(Icons.filter_list, color: Colors.white),
+                ),
+              ),
+              Text(
+                'HOME',
+                style: GoogleFonts.montserrat(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                    textStyle: TextStyle(color: Colors.white)),
+              ),
+              // Inreal case scenerio, this shouldnot be container
+              //should be wrapped with inkwell
+              Container(
+                height: 40.0,
+                width: 40.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7.0),
+                    color: Color(0xFF353535)),
+                child: Center(
+                  child: Icon(Icons.bookmark_border, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height - 150,
+          child: ListView(
+            children: <Widget>[
+              _buildListItem('images/japan.jpg', "Japan",
+                  "Description about Japan home country of One Piece"),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  _buildListItem(String imgPath, String country, String description) {
+    return Padding(
+      padding: EdgeInsets.all(15.0),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: 300.0,
+          ),
+          Container(
+            height: 300.0,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                image: DecorationImage(
+                    image: AssetImage(imgPath),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.7), BlendMode.darken))),
+          ),
+          //left at 18.15
+        ],
+      ),
+    );
   }
 }
