c70254f96634dcfae89b64421c0c8cf384b2ff1a
S J <sanjeeb.lama004@gmail.com>
2020-08-14 13:36:07 +0545

    Header completed

--- day2_ui/lib/screens/dashboard.dart
+++ day2_ui/lib/screens/dashboard.dart
@@ -1,3 +1,4 @@
+import './detailsPage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -15,7 +16,7 @@ class _DashboardState extends State<Dashboard> {
           height: 20.0,
         ),
         Padding(
-          padding: EdgeInsets.all(15.0),
+          padding: EdgeInsets.only(top: 15.0, left: 15.0, right: 15.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
@@ -51,15 +52,14 @@ class _DashboardState extends State<Dashboard> {
             ],
           ),
         ),
-        SizedBox(
-          height: 10.0,
-        ),
         Container(
           height: MediaQuery.of(context).size.height - 150,
           child: ListView(
             children: <Widget>[
-              _buildListItem('images/japan.jpg', "Japan",
-                  "Description about Japan home country of One Piece"),
+              _buildListItem(
+                  'images/japan.jpg', "Japan", "Home country of One Piece"),
+              _buildListItem(
+                  'images/canada.jpg', "Canada", "Nicest People on Earth"),
             ],
           ),
         )
@@ -86,6 +86,55 @@ class _DashboardState extends State<Dashboard> {
                         Colors.black.withOpacity(0.7), BlendMode.darken))),
           ),
           //left at 18.15
+          Container(
+            height: 300.0,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    country,
+                    style: GoogleFonts.montserrat(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                        textStyle: TextStyle(color: Colors.white)),
+                  ),
+                  Text(
+                    description,
+                    style: GoogleFonts.montserrat(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w400,
+                      textStyle: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) =>
+                              DetailsPage(imgPath: imgPath, title: country)));
+                    },
+                    child: Container(
+                      height: 50.0,
+                      width: 125.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.white),
+                      child: Center(
+                        child: Text(
+                          "Explore Now",
+                          style: GoogleFonts.montserrat(
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.bold,
+                              textStyle: TextStyle(color: Color(0xFFFD4F99))),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
