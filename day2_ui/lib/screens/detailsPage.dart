20b177f41fdff007c0134e485c439d2e39b12494
S J <sanjeeb.lama004@gmail.com>
2020-08-14 13:37:44 +0545

    Header Completed

--- day2_ui/lib/screens/detailsPage.dart
+++ day2_ui/lib/screens/detailsPage.dart
@@ -1,1 +1,78 @@
+import 'dart:ui';
 
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DetailsPage extends StatefulWidget {
+  final imgPath, title;
+
+  DetailsPage({this.imgPath, this.title});
+  @override
+  _DetailsPageState createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(widget.imgPath), fit: BoxFit.cover)),
+          ),
+          BackdropFilter(
+            filter: new ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+            child: Container(
+              decoration: BoxDecoration(color: Colors.black.withOpacity(0.5)),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(15.0, 35.0, 15.0, 15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  height: 40.0,
+                  width: 40.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(7.0),
+                      color: Color(0xFFFD4F99)),
+                  child: Center(
+                    child: Icon(
+                      Icons.filter_list,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Text(
+                  widget.title.toString().toUpperCase(),
+                  style: GoogleFonts.montserrat(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w400,
+                      textStyle: TextStyle(color: Colors.white)),
+                ),
+                Container(
+                  height: 40.0,
+                  width: 40.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(7.0),
+                      color: Color(0xFF353535)),
+                  child: Center(
+                    child: Icon(
+                      Icons.bookmark_border,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
