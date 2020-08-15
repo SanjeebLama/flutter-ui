a9f343e0212161f495eec1f2d3fa4ff58246ba81
S J <sanjeeb.lama004@gmail.com>
2020-08-15 20:09:21 +0545

    Header Completed

--- ui_3/lib/screens/indexPage.dart
+++ ui_3/lib/screens/indexPage.dart
@@ -3,6 +3,44 @@ import 'package:flutter/material.dart';
 class IndexPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                height: 150.0,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.blueAccent,
+              ),
+              Positioned(
+                  child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "Your location here",
+                      style: TextStyle(fontSize: 14.0, color: Colors.white54),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.my_location,
+                          color: Colors.white54,
+                        ),
+                        Text(
+                          "Bali Indonesia",
+                          style:
+                              TextStyle(fontSize: 14.0, color: Colors.white54),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ))
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
